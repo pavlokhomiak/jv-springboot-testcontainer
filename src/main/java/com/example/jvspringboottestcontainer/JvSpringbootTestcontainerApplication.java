75be46430e22d9c9a13e3866ef24fa4064f44b31
Pavlo Khomiak <pavlo.khomiak93@gmail.com>
2022-05-31 16:38:37 +0300

    first commit

---
+++ src/main/java/com/example/jvspringboottestcontainer/JvSpringbootTestcontainerApplication.java
@@ -0,0 +1,13 @@
+package com.example.jvspringboottestcontainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JvSpringbootTestcontainerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JvSpringbootTestcontainerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jvspringboottestcontainer/JvSpringbootTestcontainerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jvspringboottestcontainer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JvSpringbootTestcontainerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
